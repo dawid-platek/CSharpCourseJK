5113dddb674cbe9d1477930dbda7cf2cb58c6549
dawidplatek <dawidplatek.eu.pl@gmail.com>
2024-12-09 23:09:02 +0100

    Section 2 - built-in types

--- FirstProject/Program.cs
+++ FirstProject/Program.cs
@@ -11,7 +11,23 @@ namespace FirstProject
             Console.WriteLine("Hello");
             Console.WriteLine(userName);
 
+            string someText = "Some text";
+            char jChar = 'j';
+            char jCharUnicode = '\u006A';
 
+            bool isUserReady = true;
+            bool isUserNotReady = false;
+
+            DateTime now = DateTime.Now;
+            Console.WriteLine(now);
+
+            DateTime dateOfBirth = new DateTime(2000, 11, 20);
+            Console.WriteLine(dateOfBirth);
+
+            byte byteNumber = 200;
+            float floatNumber = 1.5F;
+            decimal decimalNumber = 1.5M;
+            double doubleNumber = 1.5;
 
         }
     }
